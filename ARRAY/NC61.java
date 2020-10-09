59103b95032f4d45d31042aae00c47fb0870ff82
Hua <54726816+hy2632@users.noreply.github.com>
2020-10-09 04:04:14 -0400

    NC61-twoSum

---
+++ ARRAY/NC61.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+public class NC61 {
+    /**
+     * 
+     * @param numbers int整型一维数组
+     * @param target  int整型
+     * @return int整型一维数组
+     */
+
+    public static int[] twoSum(int[] numbers, int target) {
+        // write code here
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int i=0; i<numbers.length; i++){
+            if (map.containsKey(target - numbers[i])){
+                return new int[]{map.get(target - numbers[i])+1, i+1};
+            } else {
+                map.put(numbers[i], i);
+            }
+        }
+        return null;
+    }
+
+    public static void main(String[] args){
+        int[] nums = {3,2,4};
+        int tgt = 6;
+        System.out.println(Arrays.toString(twoSum(nums, tgt)));
+    }
+
+}
